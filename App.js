9e1f0e7cb161479b31faec23d2214a512c0368f6
OmerYildiz16 <omer.yildiz.ist@gmail.com>
2019-03-04 10:46:34 +0300

    akjdhslkd

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import codePush from 'react-native-code-push'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
-type Props = {};
 export default class App extends Component<Props> {
+  
+  guncelle () { 
+
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+
+    })
+  }
+   
   render() {
     return (
       <View style={styles.container}>
